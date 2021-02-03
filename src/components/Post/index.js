18fc18be7c831c2ff8cd3d31772a455ff225516b
Smashkat12 <smashkat12@gmail.com>
2021-02-03 16:57:19 +0200

    added dynamic data into posts

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,36 +2,35 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = ({post}) => {
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         source={{
-          uri:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: post.image,
         }}
         style={styles.image}
       />
 
       {/* Bed and Bedroom */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
 
       {/* Type and Description */}
 
       <Text numberOfLines={2} style={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl
-        eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a
-        bibendum metus.
+        {post.type}. {post.title}
       </Text>
       {/* Old Price and New Price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36</Text>
-        <Text style={styles.newPrice}> $30</Text> / night
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.newPrice}> ${post.newPrice}</Text> / night
       </Text>
 
       {/* Total Price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
